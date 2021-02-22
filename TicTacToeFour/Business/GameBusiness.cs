f7ab5ecec05aa23d822d042321e065f53d85c360
ElleSowulewski <70492734+ElleSowulewski@users.noreply.github.com>
2021-02-21 22:06:17 -0500

    Initial Push

---
+++ TicTacToeFour/Business/GameBusiness.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeFour.Presentation;
+using TicTacToeFour.Models;
+using TicTacToeFour.Data;
+
+namespace TicTacToeFour.Business
+{
+    public class GameBusiness
+    {
+        public enum GameStatus
+        {
+            QUIT,
+            QUIT_SAVE
+        }
+
+        List<Player> _allPlayers;
+
+        Player _playerOne;
+        Player _playerTwo;
+
+        IDataService _dataService;
+
+        public GameBusiness()
+        {
+            InitializeDataService();
+            InitializeGame();
+        }
+
+        private void InitializeDataService()
+        {
+
+            _dataService = new DataServiceSeed();
+        }
+
+        private void InitializeGame()
+        {
+
+            _allPlayers = _dataService.ReadAll();
+
+            _playerOne = _allPlayers.FirstOrDefault(p => p.Name == "Player X");
+            _playerTwo = _allPlayers.FirstOrDefault(p => p.Name == "Player O");
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return _dataService.ReadAll();
+        }
+
+        public void SaveAllPlayers()
+        {
+            _dataService.WriteAll(_allPlayers);
+        }
+
+        public (Player playerOne, Player playerTwo) GetCurrentPlayers()
+        {
+
+            _playerOne = _allPlayers.FirstOrDefault(p => p.Name == "Player X");
+            _playerTwo = _allPlayers.FirstOrDefault(p => p.Name == "Player O");
+            (Player playerOne, Player playerTwo) currentPlayers = (_playerOne, _playerTwo);
+
+            return currentPlayers;
+        }
+    }
+}
---
+++ TicTacToeFour/Data/DataServiceSeed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeFour.Models;
+
+namespace TicTacToeFour.Data
+{
+    public class DataServiceSeed : IDataService
+    {
+        public List<Player> ReadAll()
+        {
+            return new List<Player>()
+            {
+                new Player()
+                {
+                    Name = "Player X",
+                    Wins = 0,
+                    Losses = 0,
+                    Ties = 0
+                },
+
+                new Player()
+                {
+                    Name = "Player O",
+                    Wins = 0,
+                    Losses = 0,
+                    Ties = 0
+                }
+            };
+        }
+
+        public void WriteAll(List<Player> players)
+            {
+                //
+            }
+        }
+    }
